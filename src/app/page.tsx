bb8f39f28a28753a2cd0a992520f0ab429f80274
Harmain Bashir <162583900+harmainbashir15@users.noreply.github.com>
2024-11-09 21:50:33 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+
 import Hero from "@/components/Hero";
 import Home1 from "@/components/Home1";
 import Home2 from "@/components/Home2";
